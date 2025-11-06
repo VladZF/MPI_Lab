4121227faf62e6ddd9abcbc25080a175da6cc5db
v.syrov <v.syrov@pix.ru>
2025-11-06 23:25:34 +0300

    Рефакторинг первого задания, изменение методики подсчета

--- Tasks/task1_pi.c
+++ Tasks/task1_pi.c
@@ -3,62 +3,62 @@
 #include <time.h>
 #include "mpi.h"
 
-#define HALF_SQUARE_SIZE 1
+#define HALF_SQUARE_SIZE 1.0
 
 double GetRandomDouble(const double min, const double max) {
-    if (min > max) {
-        return min;
-    }
-
     return min + ((double)rand() / RAND_MAX) * (max - min);
 }
 
-void GetPi(int comm_sz, double* result, unsigned long long points_count) {
-    unsigned long long local_points_count = points_count / comm_sz;
-    unsigned long long counter = 0;
-    double local_result = 0;
-    for (unsigned long long i = 0; i < local_points_count; i++) {
-        double x = GetRandomDouble(-HALF_SQUARE_SIZE, HALF_SQUARE_SIZE);
-        double y = GetRandomDouble(-HALF_SQUARE_SIZE, HALF_SQUARE_SIZE);
-        if (x * x + y * y <= 1) {
-            counter++;
-        }
-    }
-    local_result = (double)counter / (double)points_count * 4.0;
-    MPI_Reduce(&local_result, result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-}
-
 int main(int argc, char** argv) {
-
     MPI_Init(&argc, &argv);
 
-    unsigned long long points_count = strtoull(argv[1], NULL, 10);
-    int my_rank;
-    int comm_sz;
-
+    int my_rank, comm_sz;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
+    if (argc != 2) {
+        if (my_rank == 0) {
+            fprintf(stderr, "Использование: %s <количество_точек>\n", argv[0]);
+        }
+        MPI_Finalize();
+        return 1;
+    }
+    unsigned long long total_points = strtoull(argv[1], NULL, 10);
+
     srand(time(NULL) + my_rank);
-    
-    double parallel_result;
-    double common_time;
 
     MPI_Barrier(MPI_COMM_WORLD);
-    
-    double start = MPI_Wtime();
-    GetPi(comm_sz, &parallel_result, points_count);
-    double end = MPI_Wtime();
+    double start_time = MPI_Wtime();
 
-    double time = end-start;
-
-    MPI_Reduce(&time, &common_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    unsigned long long local_points_count = total_points / comm_sz;
 
     if (my_rank == 0) {
-        printf("%d,%lf,%lf\n", comm_sz, parallel_result, common_time);
+        local_points_count += total_points % comm_sz;
     }
-    
-    MPI_Finalize();
 
+    unsigned long long local_hits = 0;
+    for (unsigned long long i = 0; i < local_points_count; i++) {
+        double x = GetRandomDouble(-HALF_SQUARE_SIZE, HALF_SQUARE_SIZE);
+        double y = GetRandomDouble(-HALF_SQUARE_SIZE, HALF_SQUARE_SIZE);
+        if (x * x + y * y <= 1.0) {
+            local_hits++;
+        }
+    }
+
+    unsigned long long total_hits = 0;
+    MPI_Reduce(&local_hits, &total_hits, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    const double end_time = MPI_Wtime();
+    const double local_time = end_time - start_time;
+
+    double max_time;
+    MPI_Reduce(&local_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if (my_rank == 0) {
+        double pi_estimate = 4.0 * (double)total_hits / (double)total_points;
+        printf("%d,%lf,%lf\n", comm_sz, pi_estimate, max_time);
+    }
+
+    MPI_Finalize();
     return 0;
 }
